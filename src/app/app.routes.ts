b271d1b4819fb17cb15d6f194871073e0064d3fa
Amaan1122 <amaanshaikh898.as@gmail.com>
2025-07-26 00:03:25 +0530

    Seetup Header & Footer For ShhhToshi Application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { WalletComponent } from './components/wallet/wallet.component';
 import { StakingComponent } from './components/staking/staking.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { DappComponent } from './components/dapp/dapp.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'd-app', component: DappComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'wallet', component: WalletComponent },
   { path: 'staking', component: StakingComponent },
